26764402e9a1950c54ca6a4bd920c8e22e6e8797
Anthony Steffen <89876617+anthony-steffen@users.noreply.github.com>
2026-01-14 08:55:42 -0300

    🔧 chore: Configurações para deploy railway.

--- backend/src/database/config/database.js
+++ backend/src/database/config/database.js
@@ -16,7 +16,10 @@ module.exports = {
   },
 
   production: {
-    use_env_variable: 'DATABASE_URL',
+    url: process.env.MYSQL_URL || process.env.DATABASE_URL,
     dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000
+    }
   },
 };
